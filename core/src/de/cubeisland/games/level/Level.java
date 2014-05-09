5438fb43d5c430b5300ff3dbf3ba3fd38ff8c76c
Jonas Dann <jonas.dann@gmx.de>
2014-05-09 22:52:08 +0200

    Optimized the Path loading

--- core/src/de/cubeisland/games/level/Level.java
+++ core/src/de/cubeisland/games/level/Level.java
@@ -4,12 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import de.cubeisland.games.component.ComponentFactory;
 import de.cubeisland.games.component.ComponentHolder;
 import de.cubeisland.games.entity.Entity;
 import de.cubeisland.games.entity.EntityFactory;
 import de.cubeisland.games.entity.EntityType;
+import de.cubeisland.games.entity.EntityTypes;
 import de.cubeisland.games.level.tile.TileType;
 import de.cubeisland.games.wave.Difficulty;
 import de.cubeisland.games.wave.DummyWaveGenerator;
@@ -51,15 +53,22 @@ public class Level extends ComponentHolder<Level> {
 
     private TileType[][] loadMap(Pixmap rawMap) {
         TileType[][] dummyMap = new TileType[rawMap.getWidth()][rawMap.getHeight()];
+
         for (int x = 0; x < rawMap.getWidth(); x++) {
             for (int y = 0; y < rawMap.getHeight(); y++) {
                 TileType tileType = TileType.getType(rawMap.getPixel(x, y));
                 dummyMap[x][y] = tileType;
-                if (tileType == TileType.END_PATH) {
-                    getPath(dummyMap, x, y);
+                if (tileType == TileType.TOWERSLOT) {
+                    float scale = Gdx.graphics.getHeight() / dummyMap[0].length;
+                    entities.add(entityFactory.createEntity(EntityTypes.TOWER).setLocation(new Vector2((x + 0.5f) * scale, (dummyMap[0].length - 0.5f - y) * scale)));
                 }
-                else if (tileType == TileType.TOWERSLOT) {
-                    
+            }
+        }
+
+        for (int x = 0; x < dummyMap.length; x++) {
+            for (int y = 0; y < dummyMap[0].length; y++) {
+                if (dummyMap[x][y] == TileType.END_PATH) {
+                    getPath(dummyMap, x, y);
                 }
             }
         }
@@ -68,28 +77,41 @@ public class Level extends ComponentHolder<Level> {
     }
 
     private void getPath(TileType[][] map, int x, int y) {
-        getPath(map, x, y, x + 1, y + 1, new Vector2(1, 1), new ArrayList<Node>());
+        getPath(map, x, y, new Vector2(), new Vector2(), new ArrayList<Node>());
     }
-    private void getPath(TileType[][] map, int x, int y, int lastX, int lastY, Vector2 lastMove, List<Node> nodeList) {
-        if (x < 0 || x >= map.length || y < 0 || y >= map[0].length) {
-            return;
-        }
-        Vector2 currMove = new Vector2(lastX - x, lastY - y);
+    private void getPath(TileType[][] map, int x, int y, Vector2 currMove, Vector2 lastMove, List<Node> nodeList) {
         if (currMove.hasOppositeDirection(lastMove)) {
             return;
+        }
+
+        if (!(new Rectangle(0, 0, map.length - 1, map[0].length - 1).contains(x, y))) {
+            return;
+        }
+
+        if (map[x][y] == TileType.PATH && !currMove.hasSameDirection(lastMove)) {
+            nodeList.add(new Node(new Vector2(x + 0.5f + currMove.cpy().rotate(180f).x, map[0].length - (y + 0.5f) + currMove.y)));
         }
         if (map[x][y] == TileType.BEGIN_PATH) {
             nodeList.add(new Node(new Vector2(x + 0.5f, map[0].length - (y + 0.5f))));
 
             Collections.reverse(nodeList);
             paths.add(new Path(nodeList, (float) Gdx.graphics.getHeight() / (float) map[0].length));
-        } else if (map[x][y] == TileType.PATH || map[x][y] == TileType.END_PATH) {
-            nodeList.add(new Node(new Vector2(x + 0.5f, map[0].length - (y + 0.5f))));
 
-            getPath(map, x + 1, y, x, y, currMove, new ArrayList<>(nodeList));
-            getPath(map, x - 1, y, x, y, currMove, new ArrayList<>(nodeList));
-            getPath(map, x, y + 1, x, y, currMove, new ArrayList<>(nodeList));
-            getPath(map, x, y - 1, x, y, currMove, new ArrayList<>(nodeList));
+            return;
+        } else if (map[x][y] == TileType.END_PATH) {
+            nodeList.add(new Node(new Vector2(x + 0.5f, map[0].length - (y + 0.5f))));
+        }
+
+        if (map[x][y] == TileType.PATH) {
+            getPath(map, x + 1, y, new Vector2(1, 0) , currMove, new ArrayList<>(nodeList));
+            getPath(map, x - 1, y, new Vector2(-1, 0), currMove, new ArrayList<>(nodeList));
+            getPath(map, x, y + 1, new Vector2(0, 1) , currMove, new ArrayList<>(nodeList));
+            getPath(map, x, y - 1, new Vector2(0, -1), currMove, new ArrayList<>(nodeList));
+        } else if (map[x][y] == TileType.END_PATH) {
+            getPath(map, x + 1, y, new Vector2(1, 0) , new Vector2(1, 0) , new ArrayList<>(nodeList));
+            getPath(map, x - 1, y, new Vector2(-1, 0), new Vector2(-1, 0), new ArrayList<>(nodeList));
+            getPath(map, x, y + 1, new Vector2(0, 1) , new Vector2(0, 1) , new ArrayList<>(nodeList));
+            getPath(map, x, y - 1, new Vector2(0, -1), new Vector2(0, -1), new ArrayList<>(nodeList));
         }
     }
 
