a372c9eaab8563055ee803cfe8aeeb9dad1b2ac2
Phillip Schichtel <phillip.public@schich.tel>
2014-06-04 16:02:50 +0200

    added a second map

--- core/src/de/cubeisland/games/resourcemanager/MapManager.java
+++ core/src/de/cubeisland/games/resourcemanager/MapManager.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Field;
 
 public class MapManager extends ResourceManager<TileMapWithPathsAndTowerLocations> {
     public TileMapWithPathsAndTowerLocations map1;
+    public TileMapWithPathsAndTowerLocations map2;
 
     public MapManager(Chrominance game) {
         super(game, "./maps");
