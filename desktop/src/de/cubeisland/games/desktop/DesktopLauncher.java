e468454a2f878a0b98e57584c1e935785b103dd5
Phillip Schichtel <phillip.public@schich.tel>
2014-06-05 14:45:06 +0200

    disabled resizing

--- desktop/src/de/cubeisland/games/desktop/DesktopLauncher.java
+++ desktop/src/de/cubeisland/games/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.samples = 0;
+        config.resizable = false;
         new LwjglApplication(new Chrominance(), config);
     }
 }
