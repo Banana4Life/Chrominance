cfba034d059d576503602bdb717bb7ab80152d59
Phillip Schichtel <phillip.public@schich.tel>
2014-06-15 20:20:39 +0200

    rendering an actual path

--- core/src/de/cubeisland/games/component/level/PathRenderer.java
+++ core/src/de/cubeisland/games/component/level/PathRenderer.java
@@ -2,15 +2,21 @@ package de.cubeisland.games.component.level;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import de.cubeisland.games.component.After;
 import de.cubeisland.games.component.Component;
 import de.cubeisland.games.component.Phase;
 import de.cubeisland.games.level.Level;
 import de.cubeisland.games.level.Node;
 import de.cubeisland.games.level.Path;
+import de.cubeisland.games.util.Pair;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
+import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Filled;
 import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Line;
 import static de.cubeisland.games.component.TickPhase.RENDERING;
 
@@ -19,6 +25,8 @@ import static de.cubeisland.games.component.TickPhase.RENDERING;
 public class PathRenderer extends Component<Level>
 {
     private ShapeRenderer renderer;
+    private Color color = Color.BLACK;
+    private float width = 30;
 
     @Override
     protected void onInit() {
@@ -30,28 +38,59 @@ public class PathRenderer extends Component<Level>
     public void update(float delta)
     {
         List<Node> nodes;
-        Node lastNode;
-        Node currNode;
+        Vector2 previousNode;
+        Vector2 currNode;
         List<Path> paths = getOwner().getMap().getPaths();
+
+        Set<Pair<Vector2, Vector2>> lines = new HashSet<>();
 
         for (Path path : paths) {
             nodes = path.getNodes();
-            lastNode = nodes.get(0);
+            previousNode = path.getSpawn().getLocation();
             for (int j = 1; j < nodes.size(); j++) {
-                currNode = nodes.get(j);
-
-                /* renderer.setColor(Color.LIGHT_GRAY);
-                renderer.begin(Filled);
-                renderer.rectLine(currNode.getLocation(), lastNode.getLocation(), 30);
-                renderer.end(); */ // Maybe paint the paths like that
-
-                renderer.begin(Line);
-                renderer.setColor(Color.RED);
-                renderer.line(currNode.getLocation(), lastNode.getLocation());
-                renderer.end();
-
-                lastNode = currNode;
+                currNode = nodes.get(j).getLocation();
+                lines.add(new Pair<>(previousNode, currNode));
+                previousNode = currNode;
             }
         }
+
+        if (lines.isEmpty()) {
+            return;
+        }
+
+        Vector2 left, right;
+        for (Pair<Vector2, Vector2> line : lines) {
+            left = line.getLeft();
+            right = line.getRight();
+
+            renderer.begin(Filled);
+            renderer.setColor(this.color);
+            renderer.rectLine(left, right, this.width);
+            renderer.circle(left.x, left.y, this.width / 2f, (int) (this.width / 2f + .5));
+            renderer.end();
+        }
+
+        Vector2 target = paths.get(0).getTarget().getLocation();
+        renderer.begin(Filled);
+        renderer.circle(target.x, target.y, this.width / 2f, (int) (this.width / 2f + .5));
+        renderer.end();
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public PathRenderer setColor(Color color) {
+        this.color = color;
+        return this;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public PathRenderer setWidth(float width) {
+        this.width = width;
+        return this;
     }
 }
